cf72a886cd4a0160f11c7f953dde7625936a6af9
Abdullah Ubaid <Abdullahfak.au@gmail.com>
2025-04-01 18:35:11 +0700

    Try Solve 1

--- src/main/java/del/alstrudat/Program.java
+++ src/main/java/del/alstrudat/Program.java
@@ -4,5 +4,7 @@ public class Program {
 
   public static void printInfo(String name, int age) {
     // Prints the name and age of a person
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
   }
 }
