7486165141017bd8d4d2c7a8db53360282c93679
Abdullah Ubaid <Abdullahfak.au@gmail.com>
2025-04-01 17:32:26 +0700

    Init

--- src/main/java/del/alstrudat/Program.java
+++ src/main/java/del/alstrudat/Program.java
@@ -2,9 +2,7 @@ package del.alstrudat;
 
 public class Program {
 
-    public static void printInfo(String name, int age) {
-        // Print the name and age of the person
-        System.out.println("Name: " + name);
-        System.out.println("Age: " + age);
-    }
+  public static void printInfo(String name, int age) {
+    // Print the name and age of the person
+  }
 }
